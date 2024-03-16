f2d890ba47b15b4ee0a107d12c202ff8d4ec7b38
origin-coding <1604070358@qq.com>
2024-03-16 20:00:26 +0800

    feat: concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod http;
 mod server;
 
-// use std::str::FromStr;
-// use anyhow::Context;
-// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 use crate::server::Server;
 
 #[tokio::main]
 async fn main() {
-    let mut server = Server::new().await;
+    let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
-    server.run().await;
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        let server = Server::new(stream).await;
+        let future = server.run();
+        tokio::spawn(future);
+    }
 }
--- src/server/server.rs
+++ src/server/server.rs
@@ -1,25 +1,25 @@
 use std::str::FromStr;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+
 use crate::http::{HttpHeaderName, HttpHeaders, HttpRequest, HttpResponse, HttpResponseStatus, HttpVersion};
 use crate::http::HttpHeaderName::{ContentLength, ContentType};
 
 pub struct Server {
-    listener: TcpListener,
+    stream: TcpStream,
 }
 
 impl Server {
-    pub async fn new() -> Self {
-        let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
-        Self { listener }
+    pub async fn new(stream: TcpStream) -> Self {
+        Self { stream }
     }
 
-    pub async fn run(&mut self) {
+    pub async fn run(mut self) {
         loop {
             // Accept a new socket.
-            let (mut socket, _) = self.listener.accept().await.unwrap();
             let mut buffer = [0; 1024];
-            let n = socket.read(&mut buffer).await.unwrap();
+            let n = self.stream.read(&mut buffer).await.unwrap();
             let request_string = &*String::from_utf8_lossy(&buffer[..n]);
 
             // Parse the request and get response.
@@ -27,7 +27,7 @@ impl Server {
                 .and_then(Self::handle_request) {
                 // Convert response to string and write all its bytes to TcoStream.
                 let response_string = response.to_string();
-                socket.write_all(response_string.as_bytes()).await.unwrap();
+                self.stream.write_all(response_string.as_bytes()).await.unwrap();
             }
         }
     }
